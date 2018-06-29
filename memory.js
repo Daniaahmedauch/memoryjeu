3100dc4a0114cca11f226cd07a357bca83a11c5d
Daniaahmedauch <39794400+Daniaahmedauch@users.noreply.github.com>
2018-06-29 11:21:16 +0200

    memoryjs

---
+++ memory.js
@@ -0,0 +1,28 @@
+var a=0;
+var b=0;
+       $("#face0").hide();
+       $("#face1").hide();
+       $("#face2").hide();
+       $("#face3").hide();
+
+$("#dos0").click(function()
+{
+$("#dos0").hide();
+$("#face0").show();
+});
+
+$("#dos1").click(function()
+{
+$("#dos1").hide();
+$("#face1").show();
+});$("#dos2").click(function()
+{
+$("#dos2").hide();
+$("#face2").show();
+});
+$("#dos3").click(function()
+{
+$("#dos3").hide();
+$("#face3").show();
+});
+
